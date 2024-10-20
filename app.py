2058ca7ca6e79129a8047f746aae8ddb3696fe89
Thomas Raphael <thoraph96@gmail.com>
2024-10-20 09:48:33 -0300

    adicionando sistema de avaliação para os restaurantes

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Japa', 'Japonesa')
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 
-restaurante_mexicano.alternar_estado()
+restaurante_praca.alternar_estado()
+
+restaurante_praca.receber_avaliacao('Thomas', 5)
+restaurante_praca.receber_avaliacao('Evelyn', 2)
+restaurante_praca.receber_avaliacao('Marcos', 3)
 
 def main():
     Restaurante.listar_restaurantes() # chama o método que lista os restaurantes que estão guardados na lista restaurantes (atributo da classe)
--- models/restaurante.py
+++ models/restaurante.py
@@ -1,3 +1,8 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from models.avaliacao import Avaliacao
+
 class Restaurante:
     # atributos da classe (compartilhado entre instâncias)
     restaurantes = []
@@ -7,6 +12,7 @@ class Restaurante:
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacoes = []
         Restaurante.restaurantes.append(self)
     
     # define a representação em forma de string de um objeto de uma classe
@@ -16,9 +22,9 @@ class Restaurante:
     # método que itera sobre a lista de restaurantes (atributo da classe) e imprime no terminal os valores
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'.ljust(25)}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
@@ -27,3 +33,19 @@ class Restaurante:
     def alternar_estado(self):
         self._ativo = not self._ativo
         return self._ativo
+    
+    def receber_avaliacao(self, cliente, nota):
+        if 0 < nota <= 5: 
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacoes.append(avaliacao)
+        else:
+            print('Dê uma nota entre 1 e 5.')
+    
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacoes:
+            return '-'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacoes)
+        quantidade_de_notas = len(self._avaliacoes)
+        media_das_notas = round(soma_das_notas / quantidade_de_notas, 1)
+        return media_das_notas
